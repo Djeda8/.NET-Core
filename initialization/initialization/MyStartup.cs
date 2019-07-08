4556af1753da3109e2af04807a9fc5152b5b1f0c
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-07-08 23:20:15 +0200

    initial commit

---
+++ initialization/initialization/MyStartup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace initialization
+{
+    public class MyStartup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Console.WriteLine("2");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            Console.WriteLine("3");
+
+            //app.Run(async (context) =>
+            //{
+            //    var msg = $"Current environment: {env.EnvironmentName}";
+            //    await context.Response.WriteAsync(msg);
+            //});
+
+            if (env.IsDevelopment())
+            {
+                app.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("Development environment");
+                });
+            }
+            else
+            {
+                app.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("No development environment");
+                });
+            }
+        }
+    }
+}
