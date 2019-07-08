3f5fd16fb568c52b70225eaffe0ea03d41e918ce
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-07-08 14:39:39 +0200

    Add middelwares1

--- StartApp/StartApp/Startup.cs
+++ StartApp/StartApp/Startup.cs
@@ -20,12 +20,19 @@ namespace StartApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // Use 'app' to configure the pipeline
+            // Here is the initialization code
+            // Use 'app' to configure the pipeline and introduce the middlewares
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.UseErrorHandler("/html/errorpage.htm"); // Captures the errors and return the content of the static page "html / errorpage.htm"
+            app.UseStaticFiles(); // Returns the content of static files requested.
+            app.UseIdentity(); // Adds Identity Framework for managing user authentication.
+            app.UseMvcWithDefaultRoute(); // Adds MVC with a default route named 'default' and the following template: '{controller=Home}/{action=Index}/{id?}'.
 
+            // Adds a finalize middleware
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
