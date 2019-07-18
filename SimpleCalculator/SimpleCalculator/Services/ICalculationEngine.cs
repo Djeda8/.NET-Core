d0d6fd9a85b0b5e15e36d09965de080e532ac956
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-07-18 12:38:02 +0200

    initial commit

---
+++ SimpleCalculator/SimpleCalculator/Services/ICalculationEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Services
+{
+    public interface ICalculationEngine
+    {
+        int Add(int a, int b);
+        int Substract(int a, int b);
+        int Multiply(int a, int b);
+        int Divide(int a, int b);
+    }
+}
