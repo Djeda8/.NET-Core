fb2146a0a8f584a693d10361623b1f99d9271c11
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-07-19 13:10:19 +0200

    initial commit

---
+++ Caching/Caching/Middlewares/RequestCounterMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Caching.Middlewares
+{
+    public static class RequestCounterMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCounterMiddleware(
+                    this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CounterMiddleware>();
+        }
+    }
+}
