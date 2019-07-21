8d319d0a71a573e098988d872be64577c4919e00
Djeda8 <43439678+Djeda8@users.noreply.github.com>
2019-07-21 11:53:00 +0200

    initial commit

---
+++ authenticationCookies/authenticationCookies/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Security.Claims;
+
+namespace authenticationCookies
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.Name = "Auth";
+                    options.ExpireTimeSpan = TimeSpan.FromDays(30);
+                    options.SlidingExpiration = true;
+                    options.AccessDeniedPath = "/users/denied";
+                    options.LoginPath = "/users/login";
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            var cookiePolicyOptions = new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.Strict,
+            };
+
+            app.UseCookiePolicy(cookiePolicyOptions);
+
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("login/{name}/{email}", async (HttpContext context) =>
+            {
+                var name = context.GetRouteValue("name").ToString();
+                var email = context.GetRouteValue("email").ToString();
+
+                var identity = new ClaimsIdentity("MyApp");
+                identity.AddClaim(new Claim(ClaimTypes.Name, name));
+                identity.AddClaim(new Claim(ClaimTypes.Email, email));
+                identity.AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Role, "Superadmin"));
+                var principal = new ClaimsPrincipal(identity);
+
+                await context.Authentication.SignInAsync("MyApp", principal);
+
+                await HttpContext.SignInAsync(
+    CookieAuthenticationDefaults.AuthenticationScheme,
+    new ClaimsPrincipal(claimsIdentity),
+    authProperties);
+
+                await context.Response.WriteAsync("Logged in!");
+            });
+            var router = routeBuilder.Build();
+            app.UseRouter(router);
+        }
+    }
+}
