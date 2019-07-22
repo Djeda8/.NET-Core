5f6881a22ad022fe97819748a95133e41bd1162f
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-07-22 13:50:04 +0200

    initial commit

---
+++ AppInternationalization/AppInternationalization/Middelwares/HelloWorldMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace AppInternationalization.Middelwares
+{
+    public class HelloWorldMiddleware
+    {
+        private readonly IStringLocalizer<HelloWorldMiddleware> _loc;
+        public HelloWorldMiddleware(RequestDelegate next, IStringLocalizer<HelloWorldMiddleware> loc)
+        {
+            _loc = loc;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //var culture = CultureInfo.CurrentCulture.Name;
+            //await context.Response.WriteAsync($"Hello world! Current culture: {culture}");
+            var culture = CultureInfo.CurrentCulture.Name;
+            var text = _loc["Message", culture];
+            await context.Response.WriteAsync(text);
+        }
+    }
+}
