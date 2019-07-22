51260072fbf7cca9eb5d613fb72ab08b66e371b3
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-07-22 14:51:17 +0200

    initial commit

---
+++ RoutingAuthentication/RoutingAuthentication/Handlers/LoginHandlers.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutingAuthentication.Handlers
+{
+    public class LoginHandlers
+    {
+        public static async Task GetLoginPageAsync(HttpContext context)
+        {
+            var body = @"
+            <h1>Login</h1>
+            <form method='post' action='/login'>
+                Username:  <input type='text' name='username'><br>
+                Password: <input type='password' name='password'><br>
+                <hr>
+                <input type='submit' value='Login' >
+            </form>
+        ";
+            await PageUtils.SendPageAsync(context, "Login", body);
+        }
+
+        public static async Task DoLoginAsync(HttpContext context)
+        {
+            // Obtener los datos del formulario y comprobar credenciales.
+            var username = context.Request.Form["username"].ToString();
+            var password = context.Request.Form["password"].ToString();
+
+            // a) Si las credenciales son incorrectas, mostramos una página de error
+            if (String.Equals("Djeda", username))
+            {
+                context.Response.Redirect("/home");
+            }
+
+
+            // b) Si las credenciales son correctas, establecemos la cookie y redirigimos a /home 
+        }
+    }
+}
