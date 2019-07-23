6596354992d0fb161d2bef4845a9a4b116f4f44f
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-07-23 13:07:06 +0200

    initial commit

---
+++ SimpleRequests/SimpleRequests/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRequests.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Default controller with default action
+        public IActionResult Index()
+        {
+            return Content($"Welcome to my home page!");
+        }
+    }
+}
---
+++ SimpleRequests/SimpleRequests/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRequests.Controllers
+{
+    public class TestController : Controller
+    {
+        // /{controller=home}/{action=index}/{id?}
+        // /test/hello/John
+        public IActionResult Hello(string id)
+        {
+            var name = id ?? "user";
+            return Content($"Hello from ASP.NET Core MVC, {name}!");
+        }
+
+        // /{controller}/{action=index}/{id?}?age=int
+        // /test/hello/peter?age=18
+        //public IActionResult Hello(string id, int age)
+        //{
+        //    var name = id ?? "user";
+        //    return Content($"Hello , {name}, you are {age} years old");
+        //}
+
+        // /{controller}
+        // /test
+        public IActionResult Index()
+        {
+            return Content($"Testing home");
+        }
+    }
+}
