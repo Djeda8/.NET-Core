c892e48ef338d0edee4436c6efc0b4f938c77bf3
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-08-01 13:49:57 +0200

    initial commit

---
+++ InlineConstraints/InlineConstraints/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InlineConstraints.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("product/edit/{id:int}")]
+        public ActionResult GetProductById(int id)
+        {
+            return Content($"Product id: {id}");
+        }
+    }
+}
---
+++ InlineConstraints/InlineConstraints/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InlineConstraints.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
