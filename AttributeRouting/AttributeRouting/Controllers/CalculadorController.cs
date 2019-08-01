07ea2b16b8290cc10fe5ac68359fdd914a8d2d05
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-08-01 12:39:41 +0200

    initial commit

---
+++ AttributeRouting/AttributeRouting/Controllers/CalculadorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttributeRouting.Controllers
+{
+    [Route("[controller]")]
+    public class CalculatorController : Controller
+    {
+        [Route("[action]/{a}/{b}")]
+        public IActionResult Sum(int a, int b)
+        {
+            return Content($"{a}+{b}={a + b}");
+        }
+
+        [Route("[action]/product/{a}/{b?}")]
+        public IActionResult Product(int a, int b = 1)
+        {
+            return Content($"{a}*{b}={a * b}");
+        }
+    }
+}
---
+++ AttributeRouting/AttributeRouting/Controllers/SayController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttributeRouting.Controllers
+{
+    public class SayController : Controller
+    {
+        [Route("hello")]
+        public IActionResult Hello()
+        {
+            return Content("Hello!");
+        }
+    }
+}
