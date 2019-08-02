ea71c166bccda75ef1961474d2754d89a6def99f
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-08-02 14:59:12 +0200

    initial commit

---
+++ RoutingSystem/RoutingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoutingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
