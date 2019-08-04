1f382dd87ef007bf9a657d052ce31fa33fee5125
Djeda8 <43439678+Djeda8@users.noreply.github.com>
2019-08-04 10:15:56 +0200

    final commit

---
+++ Module 6 - The controller/FileUpload/FileUpload/Controllers/FileController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly string _folder;
+
+        public FileController(IHostingEnvironment hostingEnvironment)
+        {
+            _folder = Path.Combine(hostingEnvironment.WebRootPath, "uploads");
+            // Creamos "wwwroot/uploads" si no existe:
+            Directory.CreateDirectory(_folder);
+        }
+
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var filePath = Path.Combine(_folder, file.FileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+                return Content($"Uploaded {file.FileName}");
+            }
+            return Content("No file selected");
+        }
+
+        public async Task<IActionResult> Uploads(IFormFileCollection files)
+        {
+            foreach (var file in files)
+            {
+                var filePath = Path.Combine(_folder, file.FileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+            return Content($"Uploaded {files.Count} file(s)");
+        }
+    }
+}
---
+++ Module 6 - The controller/FileUpload/FileUpload/wwwroot/uploads/INewsServices.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Caching.Services
+{
+    public interface INewsServices
+    {
+        IList<string> news { get; set; }
+
+        IList<string> GetLatestNews();
+    }
+}
