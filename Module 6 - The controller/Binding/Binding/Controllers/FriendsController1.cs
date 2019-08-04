31a6ef8ab8159c039ea10005d5117ab59ab93769
Djeda8 <43439678+Djeda8@users.noreply.github.com>
2019-08-05 00:45:29 +0200

    medium commit

---
+++ Module 6 - The controller/Binding/Binding/Controllers/FriendsController1.cs
@@ -0,0 +1,23 @@
+﻿using Binding.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Binding.Controllers
+{
+    public class FriendsController1 : Controller
+    {
+        public IActionResult Show(Friend friend)
+        {
+            return Content($"{friend.Name} is {friend.Age} years old ");
+        }
+        [HttpPost]
+        public IActionResult Update([Bind("Name", "Age", Prefix = "Friend")]Friend friend)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Content("Error getting friend");
+            }
+
+            return Content($"Update {friend.Name} is {friend.Age} years old ");
+        }
+    }
+}
---
+++ Module 6 - The controller/Binding/Binding/Models/Friend.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Binding.Models
+{
+    [BindRequired]
+    public class Friend
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
