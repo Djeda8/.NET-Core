d9c613209ae07bba5cc606be1468e83d77ab6243
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-08-05 13:20:52 +0200

    initial commit

---
+++ CustomValidation/CustomValidation/Validation/IsEvenAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomValidation.Validation
+{
+    public class IsEvenAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int i = Convert.ToInt32(value);
+            return i % 2 == 0;
+        }
+    }
+}
