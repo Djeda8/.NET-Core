a0a5749d69d85f24ea89a48e35755190e32e574b
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-08-05 12:10:13 +0200

    initial commit

---
+++ BindingActions/BindingActions/Models/Friend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BindingActions.Models
+{
+    public class Friend
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public Address Address {get; set;}
+    }
+}
