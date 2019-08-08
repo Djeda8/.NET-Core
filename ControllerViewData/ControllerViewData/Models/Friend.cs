3f94f280aa461bb1575912d0f4989fe78c3e04c5
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-08-08 11:45:03 +0200

    final commit

---
+++ ControllerViewData/ControllerViewData/Models/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllerViewData.Models
+{
+    public class Friend
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
