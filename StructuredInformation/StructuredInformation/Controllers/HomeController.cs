55531c0e0d23b6ebd47130e4009631cf5e53cbd1
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-08-09 14:45:16 +0200

    final commit

---
+++ StructuredInformation/StructuredInformation/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StructuredInformation.Models;
+
+namespace StructuredInformation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            Customer customer = new Customer()
+            {
+                Name = "Daniel",
+                Phone = "620 59 47 65"
+            };
+
+            return View(customer);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
