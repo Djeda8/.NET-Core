50aca031e8082dde4d26134182bbfd957c707a17
Daniel Ojeda <43439678+Djeda8@users.noreply.github.com>
2019-08-13 14:13:50 +0200

    initial commit

---
+++ Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Filters.Models;
+using Microsoft.AspNetCore.Authorization;
+using Filters.MiddlewaresFilters;
+
+namespace Filters.Controllers
+{
+    //[Authorize]
+    [Authorize(Roles = "admin, superadmin")]
+    public class HomeController : Controller
+    {
+        //[Authorize]
+        [AllowAnonymous]
+        [MiddlewareFilter(typeof(ResponseCompressionPipeline))]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "CoolPeople")]
+        //[Authorize("CoolPeople"]
+        [Compress]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Filters/Filters/MiddlewaresFilters/CompressAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filters.MiddlewaresFilters
+{
+    public class CompressAttribute : MiddlewareFilterAttribute
+    {
+        public CompressAttribute() : base(typeof(ResponseCompressionPipeline)) { }
+        public class ResponseCompressionPipeline
+        {
+            public void Configure(IApplicationBuilder app)
+            {
+                app.UseResponseCompression();
+            }
+        }
+    }
+}
